45d671cf4bd9f78502daf71753c2f1bd970f3ba5
Hongyi <maylibooyah69@gmail.com>
2023-08-23 09:38:57 +0800

    lighting

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,9 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("scene1/",TemplateView.as_view(template_name='home/hello_webpack.html'),name='home'),
-    path("",TemplateView.as_view(template_name='home/home.html'),name='home'),
-    path("scene2",TemplateView.as_view(template_name='home/simple_animations.html'),name='home'),
-    path("scene3",TemplateView.as_view(template_name='home/textGeometry.html'),name='home'),
+    path("",TemplateView.as_view(template_name='home/home.html')),
+    path("scene2",TemplateView.as_view(template_name='home/simple_animations.html')),
+    path("scene3",TemplateView.as_view(template_name='home/textGeometry.html')),
+    path("scene4",TemplateView.as_view(template_name='home/lights.html')),
     path("font",views.send_file,name='font'),
 ]
